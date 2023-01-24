1f7d4ca2cda41c62f54c07c235c4d4bfc8e6bb1a
rohantiwari02 <123428107+rohantiwari02@users.noreply.github.com>
2023-01-24 01:20:48 -0600

    Update and rename hello.java to Hello.java

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -1,4 +1,4 @@
-public class hello {
+public class Hello {
     public static void main(String[] args) {
       String username = System.getProperty("user.name");
       System.out.println("Hello " + username + "!");
