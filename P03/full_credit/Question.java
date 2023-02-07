957d6be8880afa8123cf5dd49efff1d815a03f10
rohantiwari02 <123428107+rohantiwari02@users.noreply.github.com>
2023-02-07 09:16:40 -0600

    Update Question.java

--- P03/full_credit/Question.java
+++ P03/full_credit/Question.java
@@ -3,16 +3,14 @@ public class Question{
   private String[] answer;
   private int rightAnswer;
 
-  //private final field questionNumber
+  
   private final int questionNumber;
   private static int nextQuestionNumber = 1;
 
   Question(String string, String[] strings){
-    //initialize questionNumber
     questionNumber = nextQuestionNumber;
     nextQuestionNumber++;
     
-    //if rightAnswer is less than 1 or greater than the number of answers, throw a new IllegalArgumentException
     if(rightAnswer < 1 || rightAnswer > answer.length){
       throw new IllegalArgumentException("The right answer must be between 1 and the number of answers");
     }
