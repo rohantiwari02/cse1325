7a6eb25a5d72023c5ff135dcadf1fdae119d6bf1
rohantiwari02 <123428107+rohantiwari02@users.noreply.github.com>
2023-02-27 19:51:45 -0600

    Create Option.java

---
+++ P05/Option.java
@@ -0,0 +1,29 @@
+public class Option {
+    protected String name;
+    protected long cost;
+
+    public Option(String name, long cost) {
+        this.name = name;
+        if(cost < 0) throw new IllegalArgumentException("Price must be non-negative");
+        this.cost = cost;
+    }
+
+    public long cost() {
+        return cost;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " ($" + String.format("%.2f", (double) cost / 100) + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Option))
+            return false;
+        Option c = (Option) o;
+        return (name == c.name) && (cost == c.cost);
+    }
+}
