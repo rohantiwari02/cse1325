584449ada312d1069558cb1a2fbccb1107ab9040
rohantiwari02 <123428107+rohantiwari02@users.noreply.github.com>
2023-03-07 11:07:43 -0600

    Create Customer.java

---
+++ P06/store/Customer.java
@@ -0,0 +1,29 @@
+package store;
+
+public class Customer {
+    private String name;
+    private String email;
+
+    public Customer(String name, String email) {
+        this.name = name;
+        if (email.indexOf('@') == -1 || email.indexOf('.', email.indexOf('@')) == -1) {
+            throw new IllegalArgumentException("Invalid email address");
+        }
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + email + ")";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Customer))
+            return false;
+        Customer c = (Customer) o;
+        return (name == c.name) && (email == c.email);
+    }
+}
