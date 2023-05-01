367921fc5a43db50aac286f7a73b0caeecc05332
rohantiwari02 <rohantiwari24602@gmail.com>
2023-05-01 17:25:35 -0500

    modified:   P12/ThreadsOfSudoku.java

--- P12/ThreadsOfSudoku.java
+++ P12/ThreadsOfSudoku.java
@@ -33,14 +33,42 @@ public class ThreadsOfSudoku {
             // Your job is to rewrite this to create numThreads threads, with
             //   the set of Sudoku candidate solutions split between them
             //   (for example, 0 to 40 for the first thread and 41-81 for the second).           
-            solveSuds(0, suds.length-1, 1);
+            if(numThreads<2)
+                solveSuds(0, suds.length-1, 1);
+            else{
+                Thread[] threads = new Thread[numThreads];
+                int nextPuzzle = 0;            
+                int size = 81 / numThreads;
+                for(int i=0; i<numThreads; ++i){
+                    int endP = 80;
+                    int startP = nextPuzzle * size;
+                    if(nextPuzzle == numThreads-1){
+                        size = 81- startP;
+                        endP = 80;
+                    } else {
+                        endP = startP + size - 1;
+                    }    
+                    final int start = Math.min(startP,80);
+                    final int end = Math.min(endP,80);
+                    final int threadID = i;
+                    threads[i] = new Thread(() -> solveSuds(start, end, threadID));
+                    threads[i].start();
+                    nextPuzzle += end;
+                }
+                for(int i=0; i<numThreads; ++i){
+                    threads[i].join();
+                }
+                
+
+
+
 
             // END WORK HERE
                         
             // Show the solution(s), if any
             System.out.println("Found " + solutions.size() + " solutions");
             for(var s : solutions) System.out.println(s);
-        } catch(Exception e) {
+        }} catch(Exception e) {
             //System.err.println("ABORT: " + e);
             e.printStackTrace(System.err);
             System.exit(-1);
@@ -54,11 +82,13 @@ public class ThreadsOfSudoku {
         System.out.println("Thread " + id + " will solve " + first + " to " + last);
         for(int i=first; i<=last; ++i) {
             System.out.println("Thread " + id + " solving " + i);// + "\n\n" + suds[i]);
-            if(suds[i].solve()) solutions.add(suds[i]);
+            if(suds[i].solve()){
+                synchronized(lock){solutions.add(suds[i]);}
+            } 
         }
     }
     // END WORK HERE
-    
+    private static Object lock = new Object(); 
     private static Sudoku[] suds = new Sudoku[81];
     private static HashSet<Sudoku> solutions = new HashSet<>();
 }
